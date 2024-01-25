2dfb2386e11b1c9c0e0cc81b01ea953e6a92171d
Tim Supan <timsupann@gmail.com>
2024-01-25 12:05:20 -0800

    Add a sample image and background to setup image loading via webpack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import './style.css';
+import penguin from './images/pingu.jpeg';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = 'hello';
     element.classList.add('hello');
+
+    const myImage = new Image();
+    myImage.src = penguin;
+
+    element.appendChild(myImage);
   
     return element;
   }

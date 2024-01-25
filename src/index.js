a93d5f1308b8a8f3e8073ceed55c6b10754ccb56
Tim Supan <timsupann@gmail.com>
2024-01-25 12:00:20 -0800

    Setup image style loading for webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import './style.css';
+
 function component() {
     const element = document.createElement('div');
+    element.innerHTML = 'hello';
+    element.classList.add('hello');
   
     return element;
   }

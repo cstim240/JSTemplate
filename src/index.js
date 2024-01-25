23e33c857140bd871c39c5d5232f05893ca91c26
Tim Supan <timsupann@gmail.com>
2024-01-25 12:33:33 -0800

    Install prettier plugin for formatting code and edit .eslintrc json file for prettier extension

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './style.css';
-import penguin from './images/pingu.jpeg';
+import "./style.css";
+import penguin from "./images/pingu.jpeg";
 
 function component() {
-  const element = document.createElement('div');
-  element.innerHTML = 'hello';
-  element.classList.add('hello');
+  const element = document.createElement("div");
+  element.innerHTML = "hello";
+  element.classList.add("hello");
 
   const myImage = new Image();
   myImage.src = penguin;

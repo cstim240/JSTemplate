f1a98406655c3bec9eb32233a5f657caabeb194b
Tim Supan <timsupann@gmail.com>
2024-01-25 12:19:01 -0800

    Install ESLint for linting purposes and adjust .eslintrc.json for lint settings

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import './style.css';
 import penguin from './images/pingu.jpeg';
 
 function component() {
-    const element = document.createElement('div');
-    element.innerHTML = 'hello';
-    element.classList.add('hello');
+  const element = document.createElement('div');
+  element.innerHTML = 'hello';
+  element.classList.add('hello');
 
-    const myImage = new Image();
-    myImage.src = penguin;
+  const myImage = new Image();
+  myImage.src = penguin;
 
-    element.appendChild(myImage);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  element.appendChild(myImage);
+  return element;
+}
+
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,18 @@ module.exports = {
   },
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-        },
-        {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-        },
-        {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-        }
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
 };
